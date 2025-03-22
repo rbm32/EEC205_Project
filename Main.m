5a10fcd5b5a7c373b2a5609c4e9b46fba59ad1df
rbm32 <rmbruch@ucdavis.edu>
2025-03-22 14:20:25 -0700

    Organized Files

--- Main.m
+++ Main.m
@@ -89,65 +89,65 @@ ExecutionTime_BAYES = toc;
 mse_BR= compute_mse(g, reconstructed_bay);
 
 plot_gpr_results(g, bscan, reconstructed_bay, x, t, num_scans, -6);
-% %% Real Data
-% addpath("Data");
-% raw = csvread("Data\ryanPulse_v1.csv");
-% meta = csvread("Data\ryanPulse_v2-meta_x_t_times.csv");
-% xraw = meta(1,:);
-% xraw = xraw(xraw~=0);
-% traw = meta(2,:);
-% %% preprocessing
-% bscan_BKGR = svd_background_removal(raw, 1);
-% imagesc(xraw, traw, bscan_BKGR);
-% x_range = [-inf inf];
-% t_range = [3  7] *1e-9;
-% [bscan_cropped, x_cropped, t_cropped] = crop_bscan(bscan_BKGR, xraw, traw, x_range, t_range);
-% imagesc(x_cropped, t_cropped, bscan_cropped);
-% 
-% time_factor = 5;
-% x_factor = 5;
-% [bscan_ds, t_ds, x_ds] = downsample_bscan(bscan_cropped, t_cropped, x_cropped, time_factor, x_factor);
-% 
-% 
-% alpha = 3
-% bscan_weighted = compensate_attenuation(bscan_ds, t_ds, c, epsilon_r, alpha)
-% 
-% bscan = bscan_weighted;
-% t = t_ds;
-% x = x_ds;
-% disp(numel(bscan));
-% imagesc(x, t, bscan);
-% 
-% 
-% %%
-% pulse_sigma = 1e-9;        % Standard deviation (in same units as time)
-% pulse_duration = 6 * pulse_sigma;  % 
-% 
-% num_scans = length(x);
-% num_time_samples = length(t);
-% 
-% Ts = t(2) - t(1);
-% t_pulse = -pulse_duration/2 : Ts : pulse_duration/2;
-% pulse = exp(-t_pulse.^2 / (2 * pulse_sigma^2));
-% 
-% z = linspace(0, .5, num_time_samples);
-% 
-% epsilon_r = 2.1
-% alpha = 0;
-% h = .2;
-% c = 3e8;
-% t_max = 2e-8;
-% 
-% %% System Matrix
-% [Hr, time, x_vec, X, Z] = generate_forward_model_matrix_fast(x, z, num_time_samples, pulse, c, t_max, epsilon_r, h, alpha);
-% 
-% %% --- Reconstruction using All Methods ---
-% 
-% lambda = 0.01;
-% tic;
-% reconstructed_bay = reconstruct_g_bayesian(Hr, bscan, num_scans, num_time_samples, lambda);
-% ExecutionTime_Bayes = toc;
-% plot_g_and_bscan(reconstructed_bay, bscan, x, t, z, -11);
-% 
-% 
-% 
+%% Real Data
+addpath("Data");
+raw = csvread("Data\ryanPulse_v1.csv");
+meta = csvread("Data\ryanPulse_v2-meta_x_t_times.csv");
+xraw = meta(1,:);
+xraw = xraw(xraw~=0);
+traw = meta(2,:);
+%% preprocessing
+bscan_BKGR = svd_background_removal(raw, 1);
+imagesc(xraw, traw, bscan_BKGR);
+x_range = [-inf inf];
+t_range = [3  7] *1e-9;
+[bscan_cropped, x_cropped, t_cropped] = crop_bscan(bscan_BKGR, xraw, traw, x_range, t_range);
+imagesc(x_cropped, t_cropped, bscan_cropped);
+
+time_factor = 5;
+x_factor = 5;
+[bscan_ds, t_ds, x_ds] = downsample_bscan(bscan_cropped, t_cropped, x_cropped, time_factor, x_factor);
+
+
+alpha = 3
+bscan_weighted = compensate_attenuation(bscan_ds, t_ds, c, epsilon_r, alpha)
+
+bscan = bscan_weighted;
+t = t_ds;
+x = x_ds;
+disp(numel(bscan));
+imagesc(x, t, bscan);
+
+
+%%
+pulse_sigma = 1e-9;        % Standard deviation (in same units as time)
+pulse_duration = 6 * pulse_sigma;  % 
+
+num_scans = length(x);
+num_time_samples = length(t);
+
+Ts = t(2) - t(1);
+t_pulse = -pulse_duration/2 : Ts : pulse_duration/2;
+pulse = exp(-t_pulse.^2 / (2 * pulse_sigma^2));
+
+z = linspace(0, .5, num_time_samples);
+
+epsilon_r = 2.1
+alpha = 0;
+h = .2;
+c = 3e8;
+t_max = 2e-8;
+
+%% System Matrix
+[Hr, time, x_vec, X, Z] = generate_forward_model_matrix_fast(x, z, num_time_samples, pulse, c, t_max, epsilon_r, h, alpha);
+
+%% --- Reconstruction using All Methods ---
+
+lambda = 0.01;
+tic;
+reconstructed_bay = reconstruct_g_bayesian(Hr, bscan, num_scans, num_time_samples, lambda);
+ExecutionTime_Bayes = toc;
+plot_g_and_bscan(reconstructed_bay, bscan, x, t, z, -11);
+
+
+
